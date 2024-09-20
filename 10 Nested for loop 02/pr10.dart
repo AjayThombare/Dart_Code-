bd279e9f78a122386cf704818c71c3e50b7a9c97
AjayThombare <thombareajay2901@gmail.com>
2024-09-20 23:26:53 +0530

    Last code in Nested for loop

--- 10 Nested for loop 02/pr10.dart
+++ 10 Nested for loop 02/pr10.dart
@@ -1,7 +1,15 @@
 import "dart:io";
 void main(){
   int row = int.parse(stdin.readLineSync()!);
+  int a = 1;
+  int b = 1;
   for(int i=1; i<=row; i++){
-
+    for(int j=1; j<=i; j++){
+      stdout.write("$a"+"\t");
+      int temp = a + b;
+      a = b;
+      b = temp;
+    }
+    print("");
   }
 }
